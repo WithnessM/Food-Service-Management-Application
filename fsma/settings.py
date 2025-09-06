f4467c919505a7867c1cce298ab873dda7bdf3bb
Withness M <4023583@myuwc.ac.za>
2025-09-06 23:55:20 +0200

    Prepare settings.py for production

--- fsma/settings.py
+++ fsma/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bead6^4d2t9%!%gv*@d!jr+b56mpi2@9cuwhs2vhwx#z$*^%5+'
+import os  
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-bead6^4d2t9%!%gv*@d!jr+b56mpi2@9cuwhs2vhwx#z$*^%5+')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
