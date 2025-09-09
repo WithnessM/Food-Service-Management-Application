0f431c31dcf1f6952cb3d761d82d8eac71266fa3
Withness M <4023583@myuwc.ac.za>
2025-09-09 23:51:39 +0200

    aa

--- fsma/settings.py
+++ fsma/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Allow all hosts temporarily for EC2 deployment
 ALLOWED_HOSTS = ['*']
