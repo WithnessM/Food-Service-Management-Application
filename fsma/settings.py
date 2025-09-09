4c6f122199af3b7a0b4ffd6cca0f055e3355c6e9
Withness M <4023583@myuwc.ac.za>
2025-09-09 11:25:17 +0200

    co

--- fsma/settings.py
+++ fsma/settings.py
@@ -100,7 +100,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
